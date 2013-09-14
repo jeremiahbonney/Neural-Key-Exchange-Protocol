9c7b4c7ee9a27adf87ec703e6ed496b7b7114046
Jeremiah Bonney <jeremiahscool@yahoo.com>
2013-09-14 15:11:29 -0500

    Added outline for NKEP

--- NKEP.py
+++ NKEP.py
@@ -8,8 +8,21 @@ weight_range = 500
 party1 = TPM(input_size, hidden_node_num, weight_range)
 party2 = TPM(input_size, hidden_node_num, weight_range)
 
-def input_controller(TPM1, TPM2):
+def input_generator(TPM1, TPM2):
 	input_arr = []
 	for xrange(input_size):
 		input_arr.append(random.randint(-weight_range, weight_range))
+	return input_arr
+
+def key_exchange(TMP1, TMP2):
+	pass
+
+def hebbian_learning_rule:
+	pass
+
+def anti_hebbian_learning_rule:
+	pass
+
+def random_walk:
+	pass
 	
