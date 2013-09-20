fef3dcfc8ceff808d5cef10d0d35500351fb170d
Jeremiah Bonney <jeremiahscool@yahoo.com>
2013-09-20 11:43:37 -0500

    Added a nkep function, which prompts user for parameters for the exchange and then runs it till synchronization

--- NKEP.py
+++ NKEP.py
@@ -9,7 +9,7 @@ weight_range = 100000
 
 	
 
-def input_generator():
+def input_generator(input_size, weight_range):
 	input_arr = []
 	for y in xrange(input_size):
 		input_arr.append(random.randint(-weight_range, weight_range))
@@ -18,7 +18,7 @@ def input_generator():
 # Given two TPMs, it gives them a common output and calls the update function only if the outputs
 # are equal.
 def key_exchange(TPM1, TPM2):
-	inputs = input_generator()
+	inputs = input_generator(TPM1.input_num, TPM1.weight_range)
 	out1 = TPM1.output(inputs)
 	out2 = TPM2.output(inputs)
 	if out1 == out2:
@@ -31,7 +31,7 @@ def key_exchange(TPM1, TPM2):
 # Takes two TPM's and runs key exchange until they have equal output for 
 # cutoff number of times. 
 # Choosing a good cutoff is very important! Too small of a cutoff and the machines
-# may not be synchronized
+# may not be synchronized, but too large and "evesdroppers" may be able to sync as well.
 
 def synchronize(TPM1, TPM2, cutoff):
 	x = 0
@@ -49,6 +49,28 @@ def check_weights(TPM1, TPM2):
 		if TPM1.weights[y] != TPM2.weights[y]:
 			return -1
 	return 0
+
+
+def nkep():
+	input_size = int(raw_input("Enter the desired number of inputs for the networks\n"))
+	hidden_node_num = int(raw_input("Enter the desired number of hidden nodes. Must be able to divide the input size.\n"))
+	weight_range = int(raw_input("What weight range would you like to use? Note: Enter only one number, the range will be made up of the positive and negative versions of that number.\n"))
+	cutoff = int(raw_input("Enter a cutoff for the number of correct outputs needed to be synchronized:\n"))
+	x = TPM(input_size, hidden_node_num, weight_range)
+	y = TPM(input_size, hidden_node_num, weight_range)
+
+	while check_weights(x, y) < 0:
+		synchronize(x, y, cutoff)
+
+	print "Weights are now synced, printing them now\n"
+	print "First TPM's weights"
+	x.print_weights()
+	print "\n"
+	print "Second TPM's weights"
+	y.print_weights()
+
+
+
 
 	
 	
--- unit_test.py
+++ unit_test.py
@@ -2,24 +2,26 @@ import random
 from TPM import *
 from NKEP import *
 
-cutoff = 30
+#cutoff = 30
 
-x = TPM(100, 10, 100000)
+#x = TPM(100, 10, 100000)
 #x.print_weights()
 #x.output([1, 0, 1, -1, 1, 1, 0, -1])
-y = TPM(100, 10, 100000)
+#y = TPM(100, 10, 100000)
 #y.print_weights()
 #y.output([1, 0, 1, -1, 1, 1, 0, -1])
 
 #print input_generator()
 
-synchronize(x, y, cutoff)
-if check_weights(x, y) == -1:
-	print "Weights not synced!"
-else:
-	print "Weights are synced!"
+#synchronize(x, y, cutoff)
+#if check_weights(x, y) == -1:
+	#print "Weights not synced!"
+#else:
+#	print "Weights are synced!"
 
 
-x.print_weights()
-print "\n\n"
-y.print_weights()
+#x.print_weights()
+#print "\n\n"
+#y.print_weights()
+
+nkep()
