187a0c13b71f8fbf1c7b817c094e0d56e0d98302
Jeremiah <jeremiah@jeremiah-Satellite-L505.(none)>
2013-10-19 14:30:53 -0500

    changed weight to string function

--- enc.py
+++ enc.py
@@ -14,9 +14,9 @@ def xor_strings(str1, str2):
 
 def weight_to_string(TPM, amount = 0):
 	if amount == 0:
-		return ''.join([chr(TPM.weights[x]%95+32) for x in range(TPM.input_num)])
+		return ''.join([chr(TPM.weights[x]%255) for x in range(TPM.input_num)])
 	else:
-		return ''.join([chr(TPM.weights[x]%95+32) for x in range(amount)])
+		return ''.join([chr(TPM.weights[x]%255) for x in range(amount)])
 
 
 def encrypt(TPM, TPM_other, plaintext):
